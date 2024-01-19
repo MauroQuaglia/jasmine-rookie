8df99f603a52ee96d2c4454f94ec0d45d23db646
nautilus@gidebdevnauti02 <nautilus@trovaprezzi.it>
2024-01-19 17:30:16 +0100

    Fix

--- spec/kata/matrix_determinant_spec.js
+++ spec/kata/matrix_determinant_spec.js
@@ -1,18 +1,74 @@
 describe('Matrix determinant', function () {
     describe('minor', function () {
+        it('should return minor for a 1 x 1 matrix with index 0', function () {
+            let matrix = [[1]], index = 0;
+            expect(minor(index, matrix)).toEqual([]);
+        });
+
+        it('should return minor for a 2 x 2 matrix with index 0', function () {
+            let matrix = [[1, 2], [3, 4]], index = 0;
+            expect(minor(index, matrix)).toEqual([[4]]);
+        });
+
+        it('should return minor for a 2 x 2 matrix with index 1', function () {
+            let matrix = [[1, 2], [3, 4]], index = 1;
+            expect(minor(index, matrix)).toEqual([[3]]);
+        });
+
         it('should return minor for a 3 x 3 matrix with index 0', function () {
             let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], index = 0;
-            expect(minor(index, matrix)).toEqual([[5, 6], [8, 9]])
+            expect(minor(index, matrix)).toEqual([[5, 6], [8, 9]]);
         });
 
         it('should return minor for a 3 x 3 matrix with index 1', function () {
             let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], index = 1;
-            expect(minor(index, matrix)).toEqual([[4, 6], [7, 9]])
+            expect(minor(index, matrix)).toEqual([[4, 6], [7, 9]]);
         });
 
         it('should return minor for a 3 x 3 matrix with index 2', function () {
             let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]], index = 2;
-            expect(minor(index, matrix)).toEqual([[4, 5], [7, 8]])
+            expect(minor(index, matrix)).toEqual([[4, 5], [7, 8]]);
         });
+    });
+
+    describe('sign', function () {
+        it('should return sign for indexes', function () {
+            expect(sign(0)).toEqual(1);
+            expect(sign(1)).toEqual(-1);
+            expect(sign(2)).toEqual(1);
+            expect(sign(3)).toEqual(-1);
+        });
+    });
+
+    describe('determinant', function () {
+        it('should return determinant for 1 x 1 matrix with 0 ', function () {
+            let matrix = [[0]];
+            expect(determinant(matrix)).toEqual(0);
+        });
+
+        it('should return determinant for 1 x 1 matrix without 0 ', function () {
+            let matrix = [[1]];
+            expect(determinant(matrix)).toEqual(1);
+        });
+
+        it('should return determinant for 2 x 2 matrix with 0 ', function () {
+            let matrix = [[0, 2], [3, 4]];
+            expect(determinant(matrix)).toEqual(-6);
+
+            matrix = [[1, 0], [3, 4]];
+            expect(determinant(matrix)).toEqual(4);
+
+            matrix = [[1, 2], [0, 4]];
+            expect(determinant(matrix)).toEqual(4);
+
+            matrix = [[1, 2], [3, 0]];
+            expect(determinant(matrix)).toEqual(-6);
+        });
+
+        it('should return determinant for 2 x 2 matrix without 0 ', function () {
+            let matrix = [[1, 2], [3, 4]];
+            expect(determinant(matrix)).toEqual(-2);
+        });
+
     });
 });
--- src/kata/matrix_determinant.js
+++ src/kata/matrix_determinant.js
@@ -2,45 +2,26 @@
 
 'use strict'
 
-
-// NOTA METTERE A POSTO I -1 DEGLI INDICI!
 function determinant(matrix) {
-    // if (m.length === 1) return m[0];
-
-    let rows = matrix.length; //
-    let first_row = matrix[0]; // [a, b]
+    if (matrix.length === 1) return matrix[0][0];
 
     let result = 0;
-    for (let [item, index] of first_row) {
+    let first_row = matrix[0].entries()
+    for (let [index, item] of first_row) {
         let currentMinor = minor(index, matrix);
         result += item * sign(index) * determinant(currentMinor);
     }
     return result;
 }
 
-
 let sign = (index) => index % 2 === 0 ? 1 : -1;
 
 let minor = function (index, matrix) {
-    matrix.shift();
-    for(let row of matrix){
+    let current_matrix = JSON.parse(JSON.stringify(matrix));
+
+    current_matrix.shift();
+    for (let row of current_matrix) {
         row.splice(index, 1);
     }
-    return matrix;
+    return current_matrix;
 }
-
-//m1 = [ [4, 6], [3,8]]
-//m5 = [[2,4,2],[3,1,1],[1,2,0]]
-
-// For the 1x1 case, [ [a] ]
-// |a|
-
-// For the 2x2 case, [ [a, b], [c, d] ]
-// |a  b|
-// |c  d|
-
-
-// For the 3x3 case, [ [a, b, c], [d, e, f], [g, h, i] ] or
-// |a b c|
-// |d e f|
-// |g h i|
